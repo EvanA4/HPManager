ace29570fb248efbaa9b28cd522122e68639e0fa
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2024-12-10 12:23:45 -0500

    sketchy svelte rest api

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -1,5 +1,28 @@
+
+
 <script lang="ts">
+	interface BlogSnippet {
+    id: number;
+    title: string;
+    summary: string;
+    posted: object;
+	}
+
+  import { onMount } from 'svelte';
 	import Navbar from '../../components/Navbar.svelte';
+
+	async function getprom() {
+		let myprom = await fetch('/api/blogs');
+		let myblob = await myprom.blob();
+		return await myblob.text();
+	}
+
+	let snippets = $state<BlogSnippet[]>([]);
+	
+	onMount(async () => {
+		snippets = JSON.parse(await getprom());
+		console.log(snippets)
+	})
 </script>
 
 <!-- <div class="flex flex-row items-center justify-center h-[100vh] w-[100%]">
